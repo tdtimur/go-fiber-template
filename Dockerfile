d2f711a067bb8347b01dadbc1e4835e684a4985d
tdtimur <tahtadaritimoer@gmail.com>
2020-07-18 16:43:09 +0700

    add mongo connection test on Setup, fix docker-compose.yml

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM golang:1.14-alpine as build-deps
 WORKDIR /go/src/app
 COPY . .
 RUN mkdir build
-RUN go build -o build/server cmd/server/main.go
+RUN go build -o build/server main.go
 
 FROM alpine:latest
-COPY --from=build-deps /go/src/app/build/api-server /usr/local/bin/
-CMD ["api-server"]
+COPY --from=build-deps /go/src/app/build/server /usr/local/bin/
+CMD ["server"]
--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"context"
 	"github.com/gofiber/cors"
 	"github.com/gofiber/fiber"
 	"gitlab.com/tdtimur/go-fiber-template/routes"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"os"
+	"time"
 )
 
 func Setup() *fiber.App {
+	var pCtx = context.Background()
+	var mongoHost = os.Getenv("MONGODB_HOST")
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoHost))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(pCtx, 5*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
 	app := fiber.New()
 	corsConfig := cors.Config{
 		AllowOrigins: []string{"*"},
